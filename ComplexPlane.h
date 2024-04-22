b5c9000a9dfee1eb3486022e52183dcda8a1488e
temtem6 <t6ryspaev@gmail.com>
2024-04-22 11:42:55 -0700

    minor chnages

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -1,6 +1,7 @@
 #ifndef COMPLEXPLANE_H
 #define COMPLEXPLANE_H
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include<iostream>
 #include<vector>
 #include<complex>
@@ -23,12 +24,15 @@ class ComplexPlane : public Drawable
 public:
 	ComplexPlane(int _pixelWidth, int _pixelHeight);									  //also looks complete?, nice
 	void draw(RenderTarget& target, RenderStates states) const { target.draw(m_vArray); } //complete
-	void zoomIn();
-	void zoomOut();
-	void setCenter(Vector2i mousePixel);
-	void setMouse(Vector2i mousePixel);
+	void updateRender(); //NOT COMPLETE
+	void zoomIn(); //complete
+	void zoomOut(); //complete
+	void setCenter(Vector2i mousePixel);//complete
+	void setMouseLocation(Vector2i mousePixel); //complete
 	void loadText(Text& text);
-	void updateRender();
+	size_t countIterations(Vector2f coord);
+	void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+	Vector2f mapPixelToCoords(Vector2i mousePixel);
 private:
 	VertexArray m_vArray;
 	int pixelWidth;
@@ -36,6 +40,7 @@ private:
 	double m_aspectRatio;
 	Vector2i m_plane_center;
 	Vector2i m_plane_size;
+	Vector2i m_mouse_location;
 	int m_zoomCount;
 	State m_state;
 
